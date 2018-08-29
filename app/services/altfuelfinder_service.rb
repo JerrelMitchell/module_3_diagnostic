3daadadf6a413973c0d7cf8dff21925b3dda9a5f
JerrelMitchell <jerrelmitchell@ymail.com>
2018-08-29 11:09:20 -0600

    continue working through splitting services out of search controller (unsuccessful)

--- app/services/altfuelfinder_service.rb
+++ app/services/altfuelfinder_service.rb
@@ -1,10 +1,10 @@
 class AltfuelfinderService
-attr_reader :zip
+  attr_reader :zip
 
   def initialize(zip)
     @zip = zip
-    @conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-      faraday.headers["X-Api-Key"] = ENV["alt_fuel_finder_key"]
+    @conn = Faraday.new(url: 'https://developer.nrel.gov') do |faraday|
+      faraday.headers['X-Api-Key'] = ENV['alt_fuel_finder_key']
       faraday.adapter Faraday.default_adapter
     end
   end
