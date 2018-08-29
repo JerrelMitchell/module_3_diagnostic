44bac3d5ff16f4b332345ce7805757855ce1a4ec
JerrelMitchell <jerrelmitchell@ymail.com>
2018-08-29 09:44:47 -0600

    working through trying to set up the api request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,13 @@
 class SearchController < ApplicationController
   def index
-    station = params[:station]
+    zip = params[:zip]
 
-    @conn = Faraday.new(url: "https:///api/alt-fuel-stations") do |faraday|
-      faraday.headers["alt_fuel_finder_api_key"] = "E4uQDdiWAn5CyHH7zDzCQKSbA93h77EbeikhzKiN"
+    @conn = Faraday.new(url: "https://nrel.gov") do |faraday|
+      faraday.headers["api_key"] = "E4uQDdiWAn5CyHH7zDzCQKSbA93h77EbeikhzKiN"
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?zip")
+    response = @conn.get("/api/alt-fuel-stations/v1/nearest.json?zip=#{zip}")
 
     @stations = JSON.parse(response.body, symbolize_names: true)[:results]
   end
